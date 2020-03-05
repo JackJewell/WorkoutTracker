fe01c2321bce41bf6543f315b27f5dfe0e34f591
Jackson Jewell <jackson.t.jewell@gmail.com>
2020-03-04 22:45:27 -0600

    Established basic routing, app almost functional

--- routes/controller.js
+++ routes/controller.js
@@ -1,5 +1,7 @@
 let express = require("express");
 const path = require("path");
+const Workout = require("../models/workout");
+
 module.exports = function(app){
     app.get("/",function(req, res) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
@@ -10,4 +12,26 @@ module.exports = function(app){
     app.get("/stats",function(req, res) {
         res.sendFile(path.join(__dirname, "../public/stats.html"));
     });
+    app.post("/api/workouts", (req, res) => {
+        console.log("I'm a placeholder!")
+    });
+    
+    app.put("/api/workouts/:id",({ body }, res) =>{
+        Workout.create({ exercises: body})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+          })
+          .catch(err => {
+            res.status(400).json(err);
+          });
+      });
+    app.get("/api/workouts",(req,res) =>{
+        Workout.find({})
+        .then(dbWorkout =>{
+            res.json(dbWorkout)
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+    })
 }
